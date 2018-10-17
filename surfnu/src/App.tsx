afa0e43795145f829d4a1bb4d040ca32d0086ad4
Rasmus Lohals <rll@schultz.dk>
2018-10-18 00:33:33 +0200

    added materiel-ui and some components

--- surfnu/src/App.tsx
+++ surfnu/src/App.tsx
@@ -1,18 +1,29 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import Slide from "@material-ui/core/Slide";
+import * as React from "react";
+import "./App.css";
+import No from "./No";
+import Place from "./Place";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          <Slide
+            direction="down"
+            in={true}
+            mountOnEnter={true}
+            unmountOnExit={true}
+          >
+            <div>
+              <No />
+              <Place />
+            </div>
+          </Slide>
         </p>
       </div>
     );
---
+++ surfnu/src/Place.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Place: React.SFC<{}> = props => {
+  return <div>... ikke i Sydhavnen</div>;
+};
+
+export default Place;
